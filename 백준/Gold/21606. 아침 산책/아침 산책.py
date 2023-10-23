5f57f58010608aa2904aaaa20ecbc8ab99a45a49
LeeChanwoo <105532497+1stApplePie@users.noreply.github.com>
2023-10-23 15:31:53 +0900

    [Gold III] Title: 아침 산책, Time: 1340 ms, Memory: 132188 KB, Score: 140 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -3,40 +3,40 @@ from collections import deque
 
 input = sys.stdin.readline
 
-def DFS(graph, vertex_info, root):
+def DFS(graph, vertex_info, root, visited_outside):
     visited = dict()
     queue = deque([root])
-    count_inside = 0
-    count_path = 0
+    connected_node_list = list()
 
     while queue:
         node = queue.popleft()
         if node not in visited:
             visited.setdefault(node)
-            if vertex_info[node-1] == '1':  # 방문 노드가 실내일 때
-                count_inside += 1   # 실내 방문 횟수 1 증가
-                if count_inside == 2:   # 실내 방문 횟수가 2이면
-                    count_inside = 1
-                    count_path += 1 # 경로갯수에 1 추가
-                else:
-                    queue += graph[node, vertex_info[node-1]]   # 실내 방문 횟수가 1이면 queue에 다음 탐색 노드 추가
-            elif vertex_info[node-1] == '0': # 방문 노드가 실외이면
-                if count_inside == 1:
-                    visited.setdefault(node)
-                    queue += graph[node, vertex_info[node-1]] 
-    return count_path
+            if vertex_info[node-1] == '0':
+                visited_outside.add(node)
+                connected_node_list += graph[(node, vertex_info[node-1])]
+                queue += graph[(node, vertex_info[node-1])]
+    
+    count_inside = len(set(connected_node_list) - visited_outside)
+
+    return count_inside, visited_outside
 
 if __name__ == "__main__":
     vertex_count = int(input())
     vertex_info = input()
     graph = {(i, vertex_info[i-1]): list() for i in range(1, vertex_count+1)}
     answer = 0
+    visited_outside = set()
     for _ in range(vertex_count-1):
         vertex1, vertex2 = map(int, input().split(" "))
         graph[vertex1, vertex_info[vertex1-1]].append(vertex2)
         graph[vertex2, vertex_info[vertex2-1]].append(vertex1)
+        if vertex_info[vertex1-1] == '1' and vertex_info[vertex2-1] == '1':
+            answer += 2
 
-    for i in range(1, vertex_count+1):
-        root = i
-        answer += DFS(graph, vertex_info, root)
+    for i in range(1, vertex_count): # 마지막 node는 탐색 X
+        if i not in visited_outside and vertex_info[i-1] == '0':
+            root = i
+            count_inside, visited_outside = DFS(graph, vertex_info, root, visited_outside)
+            answer += count_inside*(count_inside-1)
     print(answer)
