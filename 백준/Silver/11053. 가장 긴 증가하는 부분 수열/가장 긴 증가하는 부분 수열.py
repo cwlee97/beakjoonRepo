e45faefcb6bd1edff5a9c9425d76621078627983
LeeChanwoo <105532497+1stApplePie@users.noreply.github.com>
2023-11-01 14:29:09 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 44 ms, Memory: 33184 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,29 +1,18 @@
 import sys
-from collections import deque
+import bisect
 
 input = sys.stdin.readline
 
-if __name__ == "__main__":
-    cases = int(input())
-    num_queue = deque(list(map(int, input().split(" "))))
+arr_length = int(input())
+arr = list(map(int, input().split(" ")))
 
-    result_list = list()
+dp = [arr[0]]
 
-    while num_queue:
-        num = num_queue.popleft()
-
-        if len(result_list) == 0:
-            result_list.append(num)
-            continue
+for i in range(arr_length):
+    if arr[i] > dp[-1]:
+        dp.append(arr[i])
+    else:
+        idx = bisect.bisect_left(dp, arr[i])
+        dp[idx] = arr[i]
     
-        if num > result_list[-1]:
-            result_list.append(num)
-
-        elif num < result_list[0]:
-            result_list[0] = num
-        
-        else:
-            for i in range(len(result_list)):
-                if result_list[i-1]< num < result_list[i]:
-                    result_list[i] = num
-    print(len(result_list))
+print(len(dp))
